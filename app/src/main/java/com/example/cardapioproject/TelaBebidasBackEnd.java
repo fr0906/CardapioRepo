ebf5f1f96bab9f3417b620edaa35a9d90126a57c
fr0906 <franciellysouzax@gmail.com>
2025-10-31 22:36:57 -0300

    criado projeto com scroll view

---
+++ app/src/main/java/com/example/cardapioproject/TelaBebidasBackEnd.java
@@ -0,0 +1,24 @@
+package com.example.cardapioproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TelaBebidasBackEnd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.tela_bebidas_front_end);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
